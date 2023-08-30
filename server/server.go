11a182f28548011df964935625cb5460c98be3f2
Sergio Fernández <sefernandez0711@gmail.com>
2023-08-30 12:00:23 -0500

    [ADD] Creamos el struc del Broker para manejar los servidores, se crear la funcion para poder heredar el conportamiento de config que tiene el struc del servido y se crea el constructor que garantiza la configuracion que se debe de tener cumpla con los datos necesarios para crear la conexion a la base de datos

--- server/server.go
+++ server/server.go
@@ -1,7 +1,52 @@
 package server
 
+import (
+	"context"
+	"errors"
+
+	"github.com/gorilla/mux"
+)
+
 type Config struct {
 	Port        string // el puerto donde se va a ejecutar
 	JWTSecret   string // una llave secreta para generar los tokens
 	DatabaseUrl string // conexion a la base de datos
+}
+
+type Server interface {
+	Config() *Config
+}
+
+type Broker struct {
+	config *Config
+	router *mux.Router
+}
+
+// crea la funcion para hacer que el struc se comporte como tipo server
+func (b *Broker) Config() *Config {
+	return b.config
+}
+
+// Constructor para el struct Broker que recibe la funcion config
+func NewServer(ctx context.Context, config *Config) (*Broker, error) {
+
+	// revisar la config para asegurar de que no tenga campos vacios
+	if config.Port == "" {
+		return nil, errors.New("port is required") // mesg: el puerto es requerido
+	}
+
+	// replicamos este comportamiento para los demas
+
+	if config.JWTSecret == "" {
+		return nil, errors.New("secret is required") // mesg: el puerto es requerido
+	}
+
+	if config.DatabaseUrl == "" {
+		return nil, errors.New("database url is required")
+	}
+
+	return &Broker{
+		config: config,
+		router: mux.NewRouter(),
+	}, nil
 }
