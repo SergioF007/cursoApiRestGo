2a90d13bb805ad2ddba377d5ad24f96a5721faeb
Sergio Fernández <sefernandez0711@gmail.com>
2023-08-30 13:15:09 -0500

    [ADD] En el Archivo main cargamos el archivó de las variables de entorno  y inicializamos las variables de entorno para hacer la conexión con el servidor de la BD, Creamos la función  BindRoutes que es el tipo de método que espera la función Start que es la que maneja la conexión.

--- handlers/home.go
+++ handlers/home.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"platzi.com/go/api/rest-ws/server"
@@ -15,5 +16,12 @@ type HomeResponse struct {
 func HomeHandler(s server.Server) http.HandlerFunc {
 	// w es la respuesta que le hacemos llegar al cliente y r es la data que nos envia el cliente
 	return func(w http.ResponseWriter, r *http.Request) {
-	
+		w.Header().Set("Content-Type", "application/json") // le definimos el header a la peticion
+		// aqui le idicamos al cliente que nostros le estamos enviando es un JSON
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(HomeResponse{
+			Message: "Welcome to Platzi GO",
+			Status:  true,
+		})
+	}
 }
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"platzi.com/go/api/rest-ws/handlers"
+	"platzi.com/go/api/rest-ws/server"
+)
+
+func main() {
+
+	// cargamos el paquete de las variables de entorno
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic("Error loading.env file")
+	}
+
+	// las capturamos en esta variables
+	PORT := os.Getenv("PORT")
+	JWT_SECRET := os.Getenv("JWT_SECRET")
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+
+	// creamos la conexion asignado los datos a a las variables correcpondiente del servidor
+	s, err := server.NewServer(context.Background(), &server.Config{
+		Port:        PORT,
+		JWTSecret:   JWT_SECRET,
+		DatabaseUrl: DATABASE_URL,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s.Start(BindRoutes)
+
+}
+
+// para poder poder poner a corrrer la conexion con la funcion Start, necesitamos el binder
+// por eso segun la estrucra que se espera creamos esta funcion
+func BindRoutes(s server.Server, r *mux.Router) {
+	r.HandleFunc("/", handlers.HomeHandler(s)).Methods(http.MethodGet)
+}
